8e8d287843793a4c1eb636c74864073e008ac01f
winmeta <winmeta.h@gmail.com>
2023-11-02 21:43:42 +0400

    nuh uh

--- Image gallery/main.js
+++ Image gallery/main.js
@@ -1,48 +1,53 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+const displayedImage = document.querySelector('.displayed-img')
+const thumbBar = document.querySelector('.thumb-bar')
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
+const btn = document.querySelector('button')
+const overlay = document.querySelector('.overlay')
 
 /* Declaring the array of image filenames */
 
-const images = ['images/pic1.jpg',
+const filenames = [
+	'images/pic1.jpg',
 	'images/pic2.jpg',
 	'images/pic3.jpg',
 	'images/pic4.jpg',
-	'images/pic5.jpg'];
+	'images/pic5.jpg',
+]
 
 /* Declaring the alternative text for each image file */
 
-const altImages = ['Closeup of a human eye',
-	'Random picture',
-	'Violet flowers',
-	'Egiptyani',
-	'Babochka'];
+const altText = [
+	'Closeup of a human eye',
+	'Some random thing',
+	'Cool flowers violets',
+	'egyptians',
+	'strashilka',
+]
 
 /* Looping through images */
 
-
-
-for (let i = 0; i < images.length; i++) {
-	const newImage = document.createElement('img');
-	newImage.setAttribute('src', images[i]);
-	newImage.setAttribute('alt', altImages[i]);
-	thumbBar.appendChild(newImage);
-	newImage.addEventListener('click', () => displayedImage.setAttribute('src', images[i]));
+for (let i = 0; i < filenames.length; i++) {
+	const newImage = document.createElement('img')
+	newImage.setAttribute('src', filenames[i])
+	newImage.setAttribute('alt', altText[i])
+	thumbBar.appendChild(newImage)
 }
 
-/* Wiring up the Darken/Lighten button */
-
-btn.addEventListener('click', () => {
-	const currentClass = btn.getAttribute('class');
-	if (currentClass === 'dark') {
-		btn.setAttribute('class', 'white');
-		btn.textContent = 'Ligthen';
-		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)';
-	} else {
-		btn.setAttribute('class', 'dark');
-		btn.textContent = 'Darken';
-		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)';
-	}
+thumbBar.addEventListener('click', event => {
+	displayedImage.setAttribute('src', event.target.getAttribute('src'))
 })
+
+/* Wiring up the Darken/Lighten button */
+function darkAndLight() {
+	if (btn.getAttribute('class') === 'dark') {
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0.5)'
+		btn.setAttribute('class', 'light')
+		btn.textContent = 'Lighten'
+	} else {
+		overlay.style.backgroundColor = 'rgba(0, 0, 0, 0)'
+		btn.setAttribute('class', 'dark')
+		btn.textContent = 'Darker'
+	}
+}
+
+btn.addEventListener('click', darkAndLight)
